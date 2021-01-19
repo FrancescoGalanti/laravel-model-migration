87f2b4d4f73f2f5315085604b9bdead222d643d3
FrancescoGalanti <galanti_francesco>
2021-01-19 14:38:04 +0100

    working

---
+++ app/Http/Controllers/Controll.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Car;
+
+class Controll extends Controller
+{
+    public function index()
+    {
+      car;
+
+      return view('home');
+    }
+}
