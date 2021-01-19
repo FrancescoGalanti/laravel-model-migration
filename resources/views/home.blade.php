72400ba5865a59d236d0f2251f1e7b9a22fd37c6
FrancescoGalanti <galanti_francesco>
2021-01-19 17:29:50 +0100

    done

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,24 @@
 
 @section('content')
   <main>
-      
+  <div>
+       <ul>
+         @foreach($cars as $item)
+          <li>
+             Marca:
+             {{$item->marca}}
+          </li>
+          <li>
+           Modello:
+          {{$item->modello}}
+          </li>
+          <li>
+           Targa:
+          {{$item->targa}}
+          <hr>
+          </li>
+         @endforeach
+       </ul>
+    </div>
   </main>
 @endsection
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,22 +1,5 @@
 <header>
     <a href="{{route('homepage')}}">casa</a>
 
-    <div>
-       <ul>
-         @foreach($cars as $item)
-          <li>
-             Marca:
-             {{$item->marca}}
-          </li>
-          <li>
-           Modello:
-          {{$item->modello}}
-          </li>
-          <li>
-           Targa:
-          {{$item->targa}}
-          </li>
-         @endforeach
-       </ul>
-    </div>
+    
 </header>
